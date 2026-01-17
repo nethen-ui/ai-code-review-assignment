07fc2a26ec5aecd7bffbab387089e0141e70fee4
Nathan Wondim <nethenwondim7@gmail.com>
2026-01-17 20:59:24 +0300

    Update correct_task1.py

--- correct_task1.py
+++ correct_task1.py
@@ -7,6 +7,8 @@ def calculate_average_order_value(orders):
             if isinstance(order["amount"], (int, float)):
                 total += order["amount"]
                 count += 1
+            else:
+                print("Adjust your dictionary and try again.")
 
     if count == 0:
         return 0
