a66378bbf96334ee00dc18fe3535c56c8bcf57c6
Nathan Wondim <nethenwondim7@gmail.com>
2026-01-17 20:49:12 +0300

    Update correct_task2.py

--- correct_task2.py
+++ correct_task2.py
@@ -2,7 +2,7 @@ import re
 
 def count_valid_emails(emails):
     count = 0
-    proper_email_format = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    proper_email_format = r"^[a-zA-Z0-9._%+-]+(?:[^\.\s]+)*@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$"
 
     for email in emails:
         if re.match(proper_email_format, email):
